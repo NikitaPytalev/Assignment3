6d8d640b206a62d62447d799825d42901a5d7aaf
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-19 22:56:25 +0300

    feat: implement api tests

---
+++ src/api.test.ts
@@ -0,0 +1,137 @@
+import 'dotenv/config';
+import request from 'supertest';
+import server from './index';
+
+
+describe('Scenario 1: Add user', () => {
+    let expectedId = '';
+    const user = {
+        username: 'test_user_1',
+        age: 100,
+        hobbies: ['test1', 'test2'],
+    };
+
+    it('GET /api/users returns status code 200 and an empty array', async () => {
+        const res = await request(server).get('/api/users')
+            .expect(200)
+            .expect('Content-Type', /json/)
+
+            expect(res.body.length).toEqual(0);
+    });
+
+    it('POST /api/users returns status code 201 and adds user', async () => {
+        const res = await request(server).post('/api/users')
+            .send(user)
+            .expect(201)
+            .expect('Content-Type', /json/)
+
+        expect(res.body).toHaveProperty('id');
+        expect(res.body.username).toEqual(user.username);
+        expect(res.body.age).toEqual(user.age);
+        expect(res.body.hobbies).toEqual(user.hobbies);
+        expectedId = res.body.id;
+    });
+
+    it('GET /api/user/{existent uuid} returns status code 200 and the user', async () => {
+        const res = await request(server).get(`/api/users/${expectedId}`)
+            .expect(200)
+            .expect('Content-Type', /json/);
+        
+        expect(res.body).toHaveProperty('id');
+        expect(res.body.username).toEqual(user.username);
+        expect(res.body.age).toEqual(user.age);
+        expect(res.body.hobbies).toEqual(user.hobbies);
+    });
+
+    afterAll(() => {
+        server.close();
+    });
+});
+
+describe('Scenario 2: Update user', () => {
+    let expectedId = '';
+    let user = {
+        username: 'test_user_1',
+        age: 100,
+        hobbies: ['test1', 'test2'],
+    };
+
+    it('POST /api/users returns status code 201 and adds user', async () => {
+        const res = await request(server).post('/api/users')
+            .send(user)
+            .expect(201)
+            .expect('Content-Type', /json/)
+
+        expect(res.body).toHaveProperty('id');
+        expect(res.body.username).toEqual(user.username);
+        expect(res.body.age).toEqual(user.age);
+        expect(res.body.hobbies).toEqual(user.hobbies);
+        expectedId = res.body.id;
+    });
+
+    user.username = 'test_user_1_changed';
+
+    it('PUT /api/users/{id} returns status code 200 and updates the user', async () => {
+        const res = await request(server).put(`/api/users/${expectedId}`)
+            .send(user)
+            .expect(200)
+            .expect('Content-Type', /json/)
+
+        expect(res.body).toHaveProperty('id');
+        expect(res.body.username).toEqual(user.username);
+        expect(res.body.age).toEqual(user.age);
+        expect(res.body.hobbies).toEqual(user.hobbies);
+        expectedId = res.body.id;
+    });
+
+    it('GET /api/user/{existent uuid} returns status code 200 and the user', async () => {
+        const res = await request(server).get(`/api/users/${expectedId}`)
+            .expect(200)
+            .expect('Content-Type', /json/);
+        
+        expect(res.body).toHaveProperty('id');
+        expect(res.body.username).toEqual(user.username);
+        expect(res.body.age).toEqual(user.age);
+        expect(res.body.hobbies).toEqual(user.hobbies);
+    });
+    
+    afterAll(() => {
+        server.close();
+    });
+});
+
+describe('Scenario 3: Delete user', () => {
+    let expectedId = '';
+    let user = {
+        username: 'test_user_1',
+        age: 100,
+        hobbies: ['test1', 'test2'],
+    };
+
+    it('POST /api/users returns status code 201 and adds a user', async () => {
+        const res = await request(server).post('/api/users')
+            .send(user)
+            .expect(201)
+            .expect('Content-Type', /json/)
+
+        expect(res.body).toHaveProperty('id');
+        expect(res.body.username).toEqual(user.username);
+        expect(res.body.age).toEqual(user.age);
+        expect(res.body.hobbies).toEqual(user.hobbies);
+        expectedId = res.body.id;
+    });
+
+    it('DELETE /api/users/{id} returns status code 204 and deletes the user', async () => {
+        const res = await request(server).delete(`/api/users/${expectedId}`)
+            .expect(204)
+    });
+
+    it('GET /api/user/{deleted uuid} returns status code 404', async () => {
+        const res = await request(server).get(`/api/users/${expectedId}`)
+            .expect(404)
+    });
+    
+    afterAll(() => {
+        server.close();
+    });
+});
--- src/data/index.ts
+++ src/data/index.ts
@@ -2,34 +2,7 @@ import User from "../models/user";
 import { v4 as uuidv4 } from 'uuid';
 import PartialUser from "../models/partialUser";
 
-let data = [
-    {
-        id: '04847fb9-8207-41c2-a5db-734be73386c7',
-        username: 'Donald Duck',
-        age: 35,
-        hobbies: [
-            'swimming',
-            'running'
-        ]
-    },
-    {
-        id: 'b38af105-153f-485f-817b-5646cbd253ec',
-        username: 'Brad Pitt',
-        age: 55,
-        hobbies: [
-            'acting',
-            'cycling'
-        ]
-    },
-    {
-        id: '9767a7f8-192e-4cac-bfc7-c18f68907d1a',
-        username: 'Elon Musk',
-        age: 50,
-        hobbies: [
-            'science'
-        ]
-    }
-];
+let data: Array<User> = [];
 
 export const getUsers = () : Promise<Array<User>> => {
     return new Promise((resolve, reject) => {
